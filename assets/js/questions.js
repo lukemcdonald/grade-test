998cd5cefe137a446f88a0f37900bb50418bb2c7
Luke McDonald <lukemcdonald@users.noreply.github.com>
2022-01-15 22:11:05 -0600

    Create questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,137 @@
+export const questions = [
+  {
+    q: "What is the hardest known mineral?",
+    a: "diamond"
+  },
+  {
+    q: "What are small deposits of igneous and metamorfic crystal?",
+    a: "gemstones"
+  },
+  {
+    q:
+      "What is any hollow space in the earth's crust that has formed naturally and is large enough for a person to enter?",
+    a: "cave"
+  },
+  {
+    q: "What is any rock containing a metal together with impurities?",
+    a: "ore"
+  },
+  {
+    q: "What are the rarest, most durable, and beautiful gems called?",
+    a: "precious stones"
+  },
+  {
+    q:
+      "What is the most commonly used practical metal; used to make tools for thousands of years?",
+    a: "iron"
+  },
+  {
+    q:
+      "What is a metal that is a good conductor of electricity; two of its alloys are brass and bronze?",
+    a: "copper"
+  },
+  {
+    q: "What is any trace of a plant or animal left in a rock?",
+    a: "fossil"
+  },
+  {
+    q:
+      "What is the area surrounding a magnet in which the force of the magnet affects other objects?",
+    a: "magnetic field"
+  },
+  {
+    q: "What are the ends of a magnet called?",
+    a: "poles"
+  },
+  {
+    q: "What is the magnetic field produced by the earth called?",
+    a: "magnetosphere"
+  },
+  {
+    q: "What is the hardest material that composes the earth's crust called?",
+    a: "rock"
+  },
+  {
+    q: "What is it called when molten solidifies and forms a rock?",
+    a: "igneous"
+  },
+  {
+    q:
+      "What are sand and minerals that have been pressed together to form what rock?",
+    a: "sedimentary"
+  },
+  {
+    q:
+      "A rock that has been changed by extreme heat or pressure into a new kind of rock is called what?",
+    a: "metamorphic"
+  },
+  {
+    q: "Over 75% of the earth's surface is covered with what rock?",
+    a: "sedimentary"
+  },
+  {
+    q:
+      "What type of igneous rock is formed at the earth's surface, cools rapidly, and has a fine grained appearance?",
+    a: "extrusive"
+  },
+  {
+    q:
+      "What type of igneous rock remains in a pocket below the surface, cools slowly, and has a coarse grained appearance?",
+    a: "intrusive"
+  },
+  {
+    q:
+      "What is the most common igneous rock and can be found everywhere on land?",
+    a: "granite"
+  },
+  {
+    q:
+      "Which sedimentary rock is formed by chemical sediments and is used for salting roads and flavoring foods?",
+    a: "halite"
+  },
+  {
+    q: "Which sedimentary rock is used to make glass?",
+    a: "sandstone"
+  },
+  {
+    q: "Which sedimentary rock is the only stone formed by living animals?",
+    a: "reef limestone"
+  },
+  {
+    q:
+      "Which metamorphic rock began as shale, is very hard, and has leaf-like layers?",
+    a: "slate"
+  },
+  {
+    q:
+      "Which metamorphic rock is perhaps one of the most beautiful and most widly used, and began as limstone?",
+    a: "marble"
+  },
+  {
+    q:
+      "What type of metamorphic rock is made up of layers like the leaves of a book?",
+    a: "foliated"
+  },
+  {
+    q: "True or False: Speleology is the study of caves.",
+    a: "true"
+  },
+  {
+    q: "True or False: Caves usually form in sandstone",
+    a: "false"
+  },
+  {
+    q:
+      "True or False: A trogloxene visits a cave regulary but must return to the surface for food.",
+    a: "true"
+  },
+  {
+    q:
+      "True or False: A lava cave is formed by the dissolving of underground rocks by water.",
+    a: "false"
+  },
+  {
+    q: "A troglophile is designed to live either ina cave or above a cave",
+    a: "true"
+  }
+];
